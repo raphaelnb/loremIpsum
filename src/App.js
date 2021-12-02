f80d75d5a76021279f022c3e6b3c683357227634
Raphael Brandão <nobre.roots@gmail.com>
2021-12-01 23:56:12 -0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import data from './data';
+
+function App() {
+    const [count,setCount] = useState(0);
+    const [text,setText] = useState([]);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log('hello wolrd');
+    }
+    return (
+        <section className="section-center">
+            <h3>Tired of boring lorem ipsum?</h3>
+            <form className="lorem-form" onSubmit={handleSubmit}>
+                <label htmlFor="amount">
+                    paragraphs:
+                </label>
+                <input type="number" name="amount" id="amount" value={count}
+                onChange={(e)=>setCount(e.target.value) }
+                />
+                <button type="submit" className="btn">generate</button>
+            </form>
+        </section>
+    );
+}
+
+export default App;
