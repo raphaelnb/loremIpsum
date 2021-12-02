caf085557c28e40bde7993c152914ea5e24ab007
Raphael Brandão <nobre.roots@gmail.com>
2021-12-02 13:46:32 -0300

    app funcionando

--- src/App.js
+++ src/App.js
@@ -7,7 +7,14 @@ function App() {
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log('hello wolrd');
+        let amount = parseInt(count);
+        if(count <= 0) {
+            amount = 1
+        }
+        if (count > 8) {
+            amount = 8
+        }
+        setText(data.slice(0,amount))
     }
     return (
         <section className="section-center">
@@ -21,6 +28,11 @@ function App() {
                 />
                 <button type="submit" className="btn">generate</button>
             </form>
+            <article className="lorem-text">
+                {text.map((item, index) => {
+                    return <p key={index}>{item}</p>;
+                })}
+            </article>
         </section>
     );
 }
